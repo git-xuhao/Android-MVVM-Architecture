9b17810fcb4fe9dc692774ba9922d5ad46e13c6b
xuhao <504105930@qq.com>
2017-12-23 19:30:49 +0800

    Initial commit

---
+++ base-frame-mvvm/src/main/java/com/hazz/baseframe/mvvm/MainActivity.java
@@ -0,0 +1,9 @@
+package com.hazz.baseframe.mvvm;
+
+/**
+ * Created by xuhao on 2017/12/22.
+ * desc:
+ */
+
+public class MainActivity {
+}
---
+++ base-frame-mvvm/src/main/java/com/hazz/baseframe/mvvm/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.hazz.baseframe.mvvm.base;
+
+/**
+ * Created by xuhao on 2017/12/22.
+ * desc:
+ */
+
+public class BaseActivity {
+}
